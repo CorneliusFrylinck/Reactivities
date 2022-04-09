2ebb04ea59182a9d6f929af677d7e4cfdfca7f09
CorneliusFrylinck <cfrylinck1997@gmail.com>
2022-04-09 17:29:00 +0200

    completed client walking skeleton

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,23 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+//import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUsers } from '@fortawesome/free-solid-svg-icons'
+import { library } from "@fortawesome/fontawesome-svg-core";
+
+library.add(faUsers);
 
 function App() {
+  const [activities, setActivities] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/activities').then(response => {
+      console.log(response);
+      var data = response.data;
+      setActivities(data);
+    });
+  }, []) //empty arr of dependencies, ensures that rendering happens once instead of an infinate loop
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div >
+      <header>
+        <h2 id="icon-container"><FontAwesomeIcon icon={["fas", "users"]} />Reactivities</h2>
+        <ul>
+          {activities.map((activity: any) => (
+              <li key={activity.id}>
+                {activity.title}
+              </li>
+          ))}
+        </ul>
       </header>
     </div>
   );
