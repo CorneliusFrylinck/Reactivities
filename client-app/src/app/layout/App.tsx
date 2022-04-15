fe0e2786670e24a1e8403b8f13a57ce9dfcb047a
CorneliusFrylinck <cfrylinck1997@gmail.com>
2022-04-15 12:52:12 +0200

    ToastContainer available to all pages

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -11,6 +11,7 @@ import HomePage from '../../features/home/HomePage';
 import ActivityForm from '../../features/activities/form/ActivityForm';
 import ActivityDetails from '../../features/activities/details/ActivityDetails';
 import TestError from '../../features/errors/TestError';
+import { ToastContainer } from 'react-toastify';
 
 library.add(faUsers);
 
@@ -19,23 +20,22 @@ function App() {
 
   return (
     <div >
-      <header>
-        <Route exact path='/' component={HomePage} />
-        <Route 
-          path={'/(.+)'}
-          render={() => (
-            <>
-              <div style={{ marginTop: '7em' }}>
-                <NavBar />
-                <Route exact path='/activities' component={ActivityDashboard} />
-                <Route path='/activities/:id' component={ActivityDetails} />
-                <Route key={location.key} path={['/createActivity', '/manage/:id']} component={ActivityForm} />
-                <Route path='/errors' component={TestError} />
-              </div>
-            </>
-          )}
-        />
-      </header>
+      <ToastContainer position='bottom-right' hideProgressBar/>
+      <Route exact path='/' component={HomePage} />
+      <Route 
+        path={'/(.+)'}
+        render={() => (
+          <>
+            <div style={{ marginTop: '7em' }}>
+              <NavBar />
+              <Route exact path='/activities' component={ActivityDashboard} />
+              <Route path='/activities/:id' component={ActivityDetails} />
+              <Route key={location.key} path={['/createActivity', '/manage/:id']} component={ActivityForm} />
+              <Route path='/errors' component={TestError} />
+            </div>
+          </>
+        )}
+      />
     </div>
   );
 }
