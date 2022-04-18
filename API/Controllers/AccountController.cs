d99f8a857d5bb2b4232999c7173a4aa7200b9ffc
CorneliusFrylinck <cfrylinck1997@gmail.com>
2022-04-18 21:39:47 +0200

    Added AccountController with user logon

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,40 @@
+using API.DTOs;
+using Domain;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AccountController : ControllerBase
+    {
+        private readonly UserManager<AppUser> _userManager;
+        public SignInManager<AppUser> _signInManager { get; }
+        public AccountController(UserManager<AppUser> userManager
+            , SignInManager<AppUser> signInManager)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+            
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<UserDto>> Login(LoginDto loginDto) {
+            var user = await _userManager.FindByEmailAsync(loginDto.Email);
+            if (user == null) return Unauthorized();
+
+            var result = await _signInManager.CheckPasswordSignInAsync(user, loginDto.Password, false);
+
+            if (result.Succeeded) {
+                return new UserDto {
+                    DisplayName = user.DisplayName,
+                    Image = null,
+                    Token = "This will be a Token",
+                    UserName = user.UserName
+                };
+            }
+            return Unauthorized();
+        }
+    }
+}
