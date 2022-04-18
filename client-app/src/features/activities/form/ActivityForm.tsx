724da5d22ba2103c7c371deb512898622e4071c5
CorneliusFrylinck <cfrylinck1997@gmail.com>
2022-04-18 13:05:18 +0200

    Added datepicker - partial type means all optional

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -11,6 +11,7 @@ import MyTextInput from "../../../app/common/form/MyTextInput";
 import MyTextArea from "../../../app/common/form/MyTextArea";
 import MySelectInput from "../../../app/common/form/MySelectInput";
 import { categoryOptions } from "../../../app/common/options/CategoryOptions";
+import MyDateInput from "../../../app/common/form/MyDateInput";
 
 export default observer(function ActivityForm() {
 
@@ -78,7 +79,13 @@ export default observer(function ActivityForm() {
                 <Form className="ui form" onSubmit={handleSubmit} autoComplete='off'>
                     <MyTextInput name='title' placeholder="Title" />
                     <MyTextArea name='description' placeholder="Description" rows={3} />
-                    <MyTextInput name='date' placeholder="Date" />
+                    <MyDateInput 
+                        name='date' 
+                        placeholderText="Date"
+                        showTimeSelect
+                        timeCaption="time"
+                        dateFormat='MMMM d, yyyy, h:mm aa'
+                    />
                     <MySelectInput options={categoryOptions} name='category' placeholder="Category" />
                     <MyTextInput name='city' placeholder="City" />
                     <MyTextInput name='venue' placeholder="Venue" />
