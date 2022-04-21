1fd495aada536d886d27fd1096342310f23de404
CorneliusFrylinck <cfrylinck1997@gmail.com>
2022-04-21 21:07:11 +0200

    Added attendees with Easy Loading

--- Application/Core/MappingProfiles.cs
+++ Application/Core/MappingProfiles.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Application.Activities;
+using Application.Profiles;
 using AutoMapper;
 using Domain;
 
@@ -12,6 +14,14 @@ namespace Application.Core
         public MappingProfiles()
         {
             CreateMap<Activity, Activity>();
+            CreateMap<Activity, ActivityDto>()
+                .ForMember(d => d.HostUserName, o =>
+                o.MapFrom(s => s.Attendees.FirstOrDefault(x => x.IsHost).AppUser.UserName));
+
+            CreateMap<ActivityAttendee, ProfileDto>()
+                .ForMember(u => u.Username, o => o.MapFrom(x => x.AppUser.UserName))
+                .ForMember(u => u.DisplayName, o => o.MapFrom(x => x.AppUser.DisplayName))
+                .ForMember(u => u.Bio, o => o.MapFrom(x => x.AppUser.Bio));
         }
     }
 }
