77f546447c0b5d5760b810cd46a4bce26e49bf70
CorneliusFrylinck <cfrylinck1997@gmail.com>
2022-04-24 16:20:11 +0200

    Add comment functionality and form validation

--- Application/Comments/CommentDto.cs
+++ Application/Comments/CommentDto.cs
@@ -11,6 +11,7 @@ namespace Application.Comments
         public DateTime CreatedAt { get; set; }
         public string? Username { get; set; }
         public string? DisplayName { get; set; }
+        public string? Body { get; set; }
         public string? Image { get; set; }
     }
 }
