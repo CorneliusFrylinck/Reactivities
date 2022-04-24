5f50fea2c3c40ddd8bcd6ad719f599874126b979
CorneliusFrylinck <cfrylinck1997@gmail.com>
2022-04-24 04:22:50 +0200

    Completed S18 - Challenge

--- client-app/src/features/profiles/ProfileCard.tsx
+++ client-app/src/features/profiles/ProfileCard.tsx
@@ -14,7 +14,9 @@ export default observer (function ProfileCard({profile} : Props) {
             <Image src={profile.image || '/assets/user.png'} />
             <Card.Content>
                 <Card.Header>{profile.displayName}</Card.Header>
-                <Card.Description>{profile.bio || ''}</Card.Description>
+                {profile.bio !== null &&
+                <Card.Description style={{ wordWrap: 'break-word', overflow: 'hidden', wordBreak: "break-all", textOverflow: 'clip', whiteSpace: "nowrap"}}>{profile.bio && profile.bio.length < 50 ? profile.bio : profile.bio + "..."}</Card.Description>
+                }
             </Card.Content>
             <Card.Content extra>
                 <Icon name="user"/>
