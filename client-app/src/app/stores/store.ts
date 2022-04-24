0c8bc01e594f6b57df1bfe800a18492bb6986670
CorneliusFrylinck <cfrylinck1997@gmail.com>
2022-04-24 15:27:48 +0200

    Added signalR comment loading to client

--- client-app/src/app/stores/store.ts
+++ client-app/src/app/stores/store.ts
@@ -1,27 +1,30 @@
 import { createContext, useContext } from "react";
 import ActivityStore from "./activityStore";
+import CommentStore from "./commentStore";
 import CommonStore from "./commonStore";
 import ModalStore from "./modalStore";
 import ProfileStore from "./profileStore";
 import UserStore from "./userStore";
 
-interface Store{
+interface Store {
     activityStore: ActivityStore;
     userStore: UserStore;
     commonStore: CommonStore;
     modalStore: ModalStore;
     profileStore: ProfileStore;
+    commentStore: CommentStore;
 }
 
-export const store: Store ={
+export const store: Store = {
     activityStore: new ActivityStore(),
     userStore: new UserStore(),
     commonStore: new CommonStore(),
     modalStore: new ModalStore(),
-    profileStore: new ProfileStore()
+    profileStore: new ProfileStore(),
+    commentStore: new CommentStore()
 }
 
-export const StoreContext=createContext(store);
+export const StoreContext = createContext(store);
 
 export function useStore() {
     return useContext(StoreContext);
