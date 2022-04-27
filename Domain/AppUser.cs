210632033e58e6ae83337af90549eba58097d283
CorneliusFrylinck <cfrylinck1997@gmail.com>
2022-04-27 13:04:31 +0200

    removed password column from AppUser

--- Domain/AppUser.cs
+++ Domain/AppUser.cs
@@ -10,7 +10,6 @@ namespace Domain
     {
         public string? DisplayName {get; set;}
         public string? Bio {get; set;}
-        public string? Password { get; set; }
         public ICollection<ActivityAttendee> Activities {get; set;} = new List<ActivityAttendee>();
         public ICollection<Photo> Photos {get; set;}
         public ICollection<UserFollowing> Followings { get; set; }
