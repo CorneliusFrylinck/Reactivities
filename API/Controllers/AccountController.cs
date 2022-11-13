a9365cc7fb4b3db6713c512a4161ca36552f9a7b
Cryptogoldenergy <cfrylinck1997@gmail.com>
2022-11-13 18:40:08 +0200

    Added confirmation in login for testing

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -52,6 +52,8 @@ namespace API.Controllers
             if (user.UserName == "narberal") user.EmailConfirmed = true;
             if (user.UserName == "shalltear") user.EmailConfirmed = true;
 
+            if (! user.EmailConfirmed) return user.EmailConfirmed = true;
+
             var result = await _signInManager.CheckPasswordSignInAsync(user, loginDto.Password, false);
 
             if (result.Succeeded) {
